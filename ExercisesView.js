013c4fadd5a552788cc08cb3620b25b732d528a7
Manya Waldia <waldia@wisc.edu>
2020-11-18 18:09:30 -0600

    exercise works

--- ExercisesView.js
+++ ExercisesView.js
@@ -20,7 +20,6 @@ class ExercisesView extends React.Component {
             },
             activities: []
         }
-
     }
 
     componentDidMount() {
@@ -30,15 +29,6 @@ class ExercisesView extends React.Component {
         })
             .then(res => res.json())
             .then(res => {
-                // console.log(Object.prototype.toString.call(res));
-                // this.setState(prevState => ({
-                //     activities: [...prevState.activities,
-                //     res.map(obj => ({
-                //         name: obj.name, duration: obj.duration,
-                //         calories: obj.calories, date: obj.date
-                //     }))
-                //     ]
-                // }));
                 this.setState({ activities: res.activities });
                 console.log(res)
             });
@@ -73,34 +63,6 @@ class ExercisesView extends React.Component {
             }));
 
     }
-    delete(item) {
-        this.setState({
-            isModalVisible: false
-        }, () => fetch('https://mysqlcs639.cs.wisc.edu/activities/' + item.id, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-access-token': this.props.accessToken
-            },
-            //one activity at a time
-            body: JSON.stringify({
-                id: item.id,
-                name: this.state.activity.name,
-                duration: this.state.activity.duration,
-                calories: this.state.activity.calories,
-                date: ''
-
-            })
-        })
-            .then(res => res.json())
-            .then(res => {
-                alert("Your profile has been updated! Refresh to see changes");
-            })
-            .catch(err => {
-                alert("Something went wrong! Verify you have filled out the fields correctly.");
-            }));
-
-    }
 
     backToLogin() {
         this.props.revokeAccessToken();
@@ -110,31 +72,30 @@ class ExercisesView extends React.Component {
 
         this.setState({
             activity:
-                { name: name }
+            {
+                ...this.state.activity,
+                name: name
+            }
         });
-        // console.log(this.state.activity)
+        console.log(this.state.activity)
     }
     onDurationChange(text) {
         this.setState({
             activity:
-                { duration: text }
+            {
+                ...this.state.activity,
+                duration: text
+            }
         });
     }
     onCaloriesChange(text) {
         this.setState({
             activity:
-                { calories: text }
-        });
-    }
-
-    deleteActivity(item) {
-        let i = 0
-        for (i = 0; i < this.state.activities.length; i++) {
-            if (item.id === this.state.activities[i].id) {
-                this.setState(this.state.activities.splice(i, 1));
-                this.delete(item)
+            {
+                ...this.state.activity,
+                calories: text
             }
-        }
+        });
     }
 
     render() {
@@ -143,91 +104,66 @@ class ExercisesView extends React.Component {
         });
         return (
 
-            // <ScrollView contentContainerStyle={{ flexGrow: 11, justifyContent: 'center', alignItems: "center" }}>
-            <View style={{ flexGrow: 1, justifyContent: 'center', alignItems: "center" }}>   
-            {/* <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}> */}
+            <View style={{ flexGrow: 1, justifyContent: 'center', alignItems: "center" }}>
+                {/* <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}> */}
                 <View style={styles.spaceSmall} />
                 <View style={styles.workout}>
                     <Button color="#942a21" title="Add Workout" onPress={() => { this.setState({ isModalVisible: true }) }} />
                 </View>
                 <View style={styles.spaceSmall} />
-                <Modal
-                    style={styles.modal}
-                    visible={this.state.isModalVisible}
-                    animationType={"fade"}
-                    transparent={false}
-                    onRequestClose={() => { console.log("Modal has been closed.") }}>
-                    {/*All views of Modal*/}
-                    <View >
-                        <View>
-                            <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Workout Name</Text>
-                        </View>
-                        <TextInput style={styles.input}
-                            underlineColorAndroid="transparent"
-                            placeholder="Running"
-                            placeholderTextColor="#d9bebd"
-                            onChangeText={(text) => this.onNameChange(text)}
-                            defaultValue={this.state.activity.name}
-                            autoCapitalize="none" />
-                        <View style={styles.spaceSmall}></View>
-                        <View>
-                            <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Calories Burned</Text>
-                        </View>
-                        <TextInput style={styles.input}
-                            underlineColorAndroid="transparent"
-                            placeholder="2200"
-                            placeholderTextColor="#d9bebd"
-                            onChangeText={(text) => this.onCaloriesChange(text)}
-                            defaultValue={this.state.activity.calories}
-                            autoCapitalize="none" />
-                        <View>
-                            <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Duration (in minutes) </Text>
-                        </View>
-                        <TextInput style={styles.input}
-                            underlineColorAndroid="transparent"
-                            placeholder="45"
-                            placeholderTextColor="#d9bebd"
-                            onChangeText={(text) => this.onDurationChange(text)}
-                            defaultValue={this.state.activity.duration}
-                            autoCapitalize="none" />
+                <View style={styles.modalspace} >
+                    <Modal
+                        style={styles.modal}
+                        visible={this.state.isModalVisible}
+                        animationType={"fade"}
+                        transparent={false}
+                        onRequestClose={() => { console.log("Modal has been closed.") }}>
+                        <View style={{ padding: 50 }}>
+                            <View>
+                                <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Workout Name</Text>
+                            </View>
+                            <TextInput style={styles.input}
+                                underlineColorAndroid="transparent"
+                                placeholder="Running"
+                                placeholderTextColor="#d9bebd"
+                                onChangeText={(text) => this.onNameChange(text)}
+                                defaultValue={this.state.activity.name}
+                                autoCapitalize="none" />
+                            <View style={styles.spaceSmall}></View>
+                            <View>
+                                <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Calories Burned</Text>
+                            </View>
+                            <TextInput style={styles.input}
+                                underlineColorAndroid="transparent"
+                                placeholder="2200"
+                                placeholderTextColor="#d9bebd"
+                                onChangeText={(text) => this.onCaloriesChange(text)}
+                                defaultValue={this.state.activity.calories}
+                                autoCapitalize="none" />
+                            <View>
+                                <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Duration (in minutes) </Text>
+                            </View>
+                            <TextInput style={styles.input}
+                                underlineColorAndroid="transparent"
+                                placeholder="45"
+                                placeholderTextColor="#d9bebd"
+                                onChangeText={(text) => this.onDurationChange(text)}
+                                defaultValue={this.state.activity.duration}
+                                autoCapitalize="none" />
+                            <View style={styles.space} />
 
-                        <View style={styles.space} />
-
-                        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-                            <Button color="#942a21" style={styles.buttonInline} title="Add Activity" onPress={() => {
-                                this.addActivity()
-                            }} />
-                            <View style={styles.spaceHorizontal} />
-                            <Button color="#942a21" style={styles.buttonInline} title="Cancel" onPress={() => {
-                                this.setState({ isModalVisible: false })
-                            }} />
+                            <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                                <Button color="#942a21" style={styles.buttonInline} title="Add Activity" onPress={() => {
+                                    this.addActivity()
+                                }} />
+                                <View style={styles.spaceHorizontal} />
+                                <Button color="#942a21" style={styles.buttonInline} title="Cancel" onPress={() => {
+                                    this.setState({ isModalVisible: false })
+                                }} />
+                            </View>
                         </View>
-                    </View>
-                </Modal>
-                <View syle={styles.list}>
-                    <FlatList
-                        style={styles.list}
-                        data={this.state.activities}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) =>
-                            <View >
-                                <Text style={styles.text}>Activity Name: {item.name}</Text>
-                                <Text style={styles.text}>Duration: {item.duration} minutes</Text>
-                                <Text style={styles.text}>Calories: {item.calories}</Text>
-                                <Text style={styles.text}>Date & Time: {moment().utcOffset('+0600').locale('en').format('YYYY-MM-DD hh:mm ')}</Text>
-                                <View style={styles.spaceSmall} />
-
-                                <View style={styles.button}>
-                                    <Button color="#942a21" style={styles.buttonInline} title="Remove Activity" onPress={() => {
-                                        this.deleteActivity(item)
-                                    }} > Remove Activity </Button>
-                                </View>
-                                <View style={styles.spaceSmall} />
-
-                            </View>}
-                    />
+                    </Modal>
                 </View>
-                <View style={styles.spaceSmall} />
                 <View style={styles.spaceSmall} />
                 <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
                     <Button color="#942a21" style={styles.buttonInline} title="Daily View" onPress={() => this.props.navigation.navigate('DailyTracker')} />
@@ -262,6 +198,10 @@ const styles = StyleSheet.create({
     space: {
         width: 20,
         height: 20,
+    },
+    modalspace: {
+        width: 50,
+        height: 50
     },
     spaceHorizontal: {
         display: "flex",
@@ -303,7 +243,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 1,
         borderColor: '#fff',
-        margin: 0
+        margin: 50
     },
     list: {
         fontWeight: 'bold',
